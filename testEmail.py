eeb5344b87571774ce9a5cbeadc53b8418dab433
Fatin Aisyah Binti Ghazali <aisyah.fatin@gmail.com>
2018-05-17 12:11:56 +0800

    new code

--- testEmail.py
+++ testEmail.py
@@ -1,20 +1,16 @@
 import smtplib
-import getpass
 
-print("Please login to proceed")
-email = input('E-mail: ')
-password = getpass.getpass('Password: ')
-
-
-print("Logged in\n")
-receiver = input('Receiver e-mail: ')
-content = input('Enter your message here:\n')
+#content = 'This message is sent through python codes'
+receiver = input('Enter receiver email: ')
+content = input ('Your message: ') 
 
 mail = smtplib.SMTP('smtp.gmail.com',587)
-subject = 'Sent from python programming' 
+#subject = 'Sent from python programming' 
 mail.ehlo()
 mail.starttls()
-mail.login('email','password')
-mail.sendmail('email','receiver',content)
+mail.login('sisterofthecheater@gmail.com','malaysiaku123')
+
+
+mail.sendmail('sisterofthecheater@gmail.com',receiver,content)
 
 mail.close()
