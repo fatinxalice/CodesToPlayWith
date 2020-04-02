0843cc5008449d465540942c5908159cd41ddb56
fatinxalice <aisyah.fatin@gmail.com>
2020-04-02 13:25:25 +0800

    summoning servant for HOly Grail War

---
+++ servant.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+int main(){
+	
+	char y_n; 
+	printf("<<< Servant Summoning Circle >>>\n Ready to summon your servant(y/n)? ");
+	scanf("%c",&y_n);
+
+	if(y_n == 'n'){
+		printf("Kden");
+	}
+
+}
+	
